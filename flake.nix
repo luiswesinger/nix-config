a3c8c25abcbffd168b999f147c8ab3792eec91e2
Luis Wesinger <wesingerluis@gmail.com>
2025-09-01 21:43:57 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    agenix.url = "github:ryantm/agenix";
+
+    mysecrets = { url = "git@github.com:luiswesinger/secrets"; flake = false; };
+  };
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      first-test = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # Set all input parameters as specialArgs of all sub-modules
+        # so that we can use the `agenix` & `mysecrets` in sub-modules
+        specialArgs = inputs;
+        modules = [
+	 ./configuration.nix
+          # import & decrypt secrets in `mysecrets` in this module
+          ./secrets/default.nix
+        ];
+      };
+    };
+  };
+}
