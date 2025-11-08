8e092a6c0cc08ee6f2cd998e055a9133f61e6a33
luis <wesingerluis@gmail.com>
2025-11-08 16:51:41 +0100

    added ai cli tools

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -1,8 +1,14 @@
 # /home/apps/default.nix
+{ pkgs, ... }:
+
 {
   imports = [
     # ./geckodriver.nix
     ./zathura.nix
   ];
 
+  
+  home.packages = with pkgs; [
+    # chatgpt 
+  ];
 }
--- home/cli/cli_tools.nix
+++ home/cli/cli_tools.nix
@@ -1,8 +1,11 @@
+# /home/cli/cli_tools.nix
 { pkgs, ... }:
 
 {
   home.packages = with pkgs; [
     pomodoro
     ripgrep
+    gemini-cli
+    chatgpt-cli
   ];
 }
