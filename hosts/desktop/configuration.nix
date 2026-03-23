e67481aae4a9f23c9f0554619578269d0da218ad
luis <wesingerluis@gmail.com>
2026-03-23 16:44:31 +0100

    deactivated hyprland

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -7,7 +7,7 @@
     ./additional-hardware-config.nix
    
     ../../modules/desktop_environment/kdeplasma6.nix
-    ../../modules/desktop_environment/hyprland.nix
+    #../../modules/desktop_environment/hyprland.nix
 
     ../../modules/system/base/default.nix
 
