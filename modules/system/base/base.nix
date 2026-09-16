9e4a8866ded2f7e32351101916da071ce3702fea
luis <wesingerluis@gmail.com>
2026-09-16 15:07:43 +0200

    updated flake and minor changes to base.nix

--- modules/system/base/base.nix
+++ modules/system/base/base.nix
@@ -61,7 +61,7 @@
   in {
     settings = {
       # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
+      experimental-features = ["nix-command" "flakes"];
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
     };
